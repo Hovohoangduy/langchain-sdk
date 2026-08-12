c9e7cc658e779a1794ca20c9fa298b76784234a2
Ho Vo Hoang Duy - Fresher AI Engineer <h.hoangduy2002@gmail.com>
2026-08-12 10:29:46 +0700

    updates

--- core_components/stm.py
+++ core_components/stm.py
@@ -58,3 +58,128 @@ response = agent.invoke(
     {"configurable": {"thread_id": "1"}}
 )
 print(response["messages"][-1].content)
+
+
+###
+## Trim message
+from langchain.messages import RemoveMessage
+from langgraph.graph.message import REMOVE_ALL_MESSAGES
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain.agents import create_agent, AgentState
+from langchain.agents.middleware import before_model
+from langgraph.runtime import Runtime
+from langchain_core.runnables import RunnableConfig
+from typing import Any
+
+@before_model
+def trim_messages(state: AgentState, runtime: Runtime) -> dict[str, Any] | None:
+    """Keep only the last few messages to fit context window."""
+    messages = state["messages"]
+
+    if len(messages) <= 3:
+        return None
+
+    first_msg = messages[0]
+    recent_messages = messages[-3:] if len(messages) % 2 == 0 else messages[-4:]
+    new_messages = state["messages"]
+
+    return {
+        "messages": [
+            RemoveMessage(id=REMOVE_ALL_MESSAGES),
+            *new_messages
+        ]
+    }
+
+agent = create_agent(
+    "gpt-5.4-mini",
+    tools=[],
+    middleware=[trim_messages],
+    checkpointer=InMemorySaver(),
+)
+
+config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+agent.invoke({"messages": "hi, my name is bob"}, config)
+agent.invoke({"messages": "write a short poem about cat"}, config)
+agent.invoke({"messages": "now do the same but for dogs"}, config)
+final_response = agent.invoke({"messages": "What's my name?"}, config)
+
+final_response["messages"][-1].pretty_print()
+
+
+### 
+## Delete message
+from langchain.messages import RemoveMessage
+from langchain.agents import create_agent, AgentState
+from langchain.agents.middleware import after_model
+from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.runtime import Runtime
+from langchain_core.runnables import RunnableConfig
+
+@after_model
+def delete_old_messages(state: AgentState, runtime: Runtime) -> dict | None:
+    """Remove old messages to keep conversation manageable"""
+    messages = state["messages"]
+    if len(messages) > 2:
+        # remove the earlisest two message
+        return {"messages": [RemoveMessage(id=m.id) for m in messages[:2]]}
+    return None
+
+agent = create_agent(
+    "gpt-5-nano",
+    tools=[],
+    system_prompt="Please be concise and to the point.",
+    middleware=[delete_old_messages],
+    checkpointer=InMemorySaver(),
+)
+
+config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+
+stream = agent.stream_events(
+    {"messages": [{"role": "user", "content": "hi, I'm bob"}]},
+    config,
+    version="v3"
+)
+
+for snapshot in stream.values:
+    print([(message.type, message.content) for message in snapshot["messages"]])
+
+stream = agent.stream_events(
+    {"messages": [{"role": "user", "content": "what's is my name?"}]},
+    config,
+    version="v3"
+)
+
+for snapshot in stream.values:
+    print([(message.type, message.content) for message in snapshot["messages"]])
+
+
+###
+## Sumarize message
+from langchain.agents import create_agent
+from langchain.agents.middleware import SummarizationMiddleware
+from langgraph.checkpoint.memory import InMemorySaver
+from langchain_core.runnables import RunnableConfig
+
+checkpointer = InMemorySaver()
+
+agent = create_agent(
+    "gpt-5.4-mini",
+    tools=[],
+    middleware=[
+        SummarizationMiddleware(
+            model="gpt-5.4-mini",
+            trigger=("tokens", 4000),
+            keep=("messages", 20)
+        )
+    ],
+    checkpointer=checkpointer,
+)
+
+config: RunnableConfig = {"configurable": {"thread_id": "1"}}
+agent.invoke({"messages": "hi, my name is bob"}, config)
+agent.invoke({"messages": "write a short poem about cats"}, config)
+agent.invoke({"messages": "now do the same but for dogs"}, config)
+final_response = agent.invoke({"messages": "what's my name?"}, config)
+
+final_response["messages"][-1].pretty_print()
